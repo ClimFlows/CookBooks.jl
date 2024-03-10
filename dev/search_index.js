95ac6a1de3f1981ccd099d40518bc3ec51cdf2bf
Documenter.jl <documenter@juliadocs.github.io>
2024-03-10 00:42:24 +0000

    build based on 9c00e11

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CookBooks","category":"page"},{"location":"#CookBooks","page":"Home","title":"CookBooks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CookBooks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CookBooks]","category":"page"}]
+}
